c2a3db77bbf3b35347e715afd521ea5a91224cef
mnyesayev <m.n.y.121416@gmail.com>
2020-10-19 14:52:29 +0300

    add projact dotNet5781_00_3729_1237

---
+++ dotNet5781_00_3729_1237/Program3729.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3729_1237
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3729();
+            Welcome1237();
+            Console.ReadKey();
+        }
+        static partial void Welcome1237();
+        private static void Welcome3729()
+        {
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", userName);
+        }
+    }
+}
