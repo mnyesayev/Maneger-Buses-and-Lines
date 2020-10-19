08949c10a9cd2b950d5f1a5a024da401ff94f03b
david kam <idavidkam@gmail.com>
2020-10-19 15:13:55 +0300

    add new class "program1237"

---
+++ dotNet5781_00_3729_1237/Program1237.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_3729_1237
+{
+    partial class program
+    {
+        static partial void Welcome1237()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
