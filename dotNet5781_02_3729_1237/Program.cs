a37b4c9d5b37cb4f38225a2793b0248ab78e9996
mnyesayev <m.n.y.121416@gmail.com>
2020-11-08 11:40:03 +0200

    I add menu to program

--- dotNet5781_02_3729_1237/Program.cs
+++ dotNet5781_02_3729_1237/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace dotNet5781_02_3729_1237
 {
+    public enum Choises { exit, addLine, addStation, delLine, delStation, search, noReplacements, printLines, printStations };
     class Program
     {
         static void Main(string[] args)
         {
+            string[] options = new string[]
+            {
+                "Exit the program",
+                "Add a new line",
+                "Add a new station to an existing line",
+                "Delete an existing line",
+                "Delete a station from an existing line",
+                "Search lines that pass through the station by station number",
+                "To find a route without changing buses",
+                "Print all the lines in the system",
+                "Print all the stations in the system and the lines that pass through them"
+            };
+            Choises userChoise;
+
             BusLineStation[] stations = new BusLineStation[5];
             Random r = new Random(DateTime.Now.Millisecond);
             for (int i = 0; i < 5; i++)
@@ -20,11 +35,45 @@ namespace dotNet5781_02_3729_1237
             int j;
             for (int i = 0; i < 5; i++)
             {
-                j=int.Parse(Console.ReadLine());
+                j = int.Parse(Console.ReadLine());
                 st.addStation(stations[i], j);
             }
-
-                Console.WriteLine(st.ToString());
+            Console.WriteLine(st.ToString());
+            do
+            {
+                for (int i = 0; i < options.Length; i++)
+                    Console.WriteLine("Press {0} to " + options[i], i);
+                
+                // get the number
+                int temp;
+                while (!int.TryParse(Console.ReadLine(), out temp))
+                    Console.WriteLine("Wrong input! Try Again.");
+                userChoise = (Choises)temp;
+                
+                switch (userChoise)
+                {
+                    case Choises.exit:
+                        break;
+                    case Choises.addLine:
+                        break;
+                    case Choises.addStation:
+                        break;
+                    case Choises.delLine:
+                        break;
+                    case Choises.delStation:
+                        break;
+                    case Choises.search:
+                        break;
+                    case Choises.noReplacements:
+                        break;
+                    case Choises.printLines:
+                        break;
+                    case Choises.printStations:
+                        break;
+                    default:
+                        break;
+                }
+            } while (userChoise != Choises.exit);
             Console.ReadKey();
         }
     }
