b90e252ee1867d3f07fd991b752ef876621a3e50
mnyesayev <m.n.y.121416@gmail.com>
2020-11-11 12:20:17 +0200

    fix some things

--- dotNet5781_02_3729_1237/BusLineStation.cs
+++ dotNet5781_02_3729_1237/BusLineStation.cs
@@ -7,18 +7,18 @@ using System.Threading.Tasks;
 
 namespace dotNet5781_02_3729_1237
 {
-    public class BusLineStation 
+    public class BusLineStation:BusStation 
     {
         private double distancePrevStation;
         private TimeSpan minutesTimePrevStation;
-        BusStation busStation;
         public double DistancePrevStation { get => distancePrevStation; set => distancePrevStation = value; }
         public TimeSpan MinutesTimePrevStation { get => minutesTimePrevStation; set => minutesTimePrevStation = value; }
-        public BusStation BusStation { get => busStation; set => busStation = value; }
-
         public BusLineStation(BusStation busStation, double distance = 0, double minutsTime = 0)
         {
-            BusStation = busStation;
+            base.Latitude = busStation.Latitude;
+            base.Longitude = busStation.Longitude;
+            base.BusStationKey = busStation.BusStationKey;
+            base.Address = busStation.Address;
             DistancePrevStation = distance;
             MinutesTimePrevStation = TimeSpan.FromMinutes(minutsTime);
         }
