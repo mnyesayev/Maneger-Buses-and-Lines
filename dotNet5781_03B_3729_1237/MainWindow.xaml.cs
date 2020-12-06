641c40dd463502d8b3a9e9bfaad012e3976ac4ee
david kam <idavidkam@gmail.com>
2020-12-06 19:21:40 +0200

    wDelBus bug fix

--- dotNet5781_03B_3729_1237/MainWindow.xaml.cs
+++ dotNet5781_03B_3729_1237/MainWindow.xaml.cs
@@ -273,7 +273,8 @@ namespace dotNet5781_03B_3729_1237
                     buses.Buses.Remove(busToDel);
                 else return;
             }
-            else MessageBox.Show("The bus not exsist ", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            else if (busToDel != null)
+                MessageBox.Show("The bus not exsist ", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
     }
