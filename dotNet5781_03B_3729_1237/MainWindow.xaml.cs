cc5a95624ae7300b0912ecad62dc68d95281e324
mnyesayev <m.n.y.121416@gmail.com>
2020-12-06 19:32:47 +0200

    fix bug delBus

--- dotNet5781_03B_3729_1237/MainWindow.xaml.cs
+++ dotNet5781_03B_3729_1237/MainWindow.xaml.cs
@@ -273,7 +273,7 @@ namespace dotNet5781_03B_3729_1237
                     buses.Buses.Remove(busToDel);
                 else return;
             }
-            else if (busToDel != null)
+            else 
                 MessageBox.Show("The bus not exsist ", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
         }
 
