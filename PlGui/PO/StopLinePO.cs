9045486465dd4e3ef32d4b4000b179e7987183ad
david kam <idavidkam@gmail.com>
2020-12-30 19:44:35 +0200

    add to PO user, stopLine

---
+++ PlGui/PO/StopLinePO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PO
+{
+    public class StopLine: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    {
+        /// <summary>
+        /// Represents the inner unique number of the "Line"
+        /// </summary>
+        public int IdLine { get; set; }
+        /// <summary>
+        /// Represents the unique code of the "BusStop"
+        /// </summary>
+        public int CodeStop { get; set; }
+        /// <summary>
+        /// Represents the number Stop/station in line
+        /// </summary>
+        public int NumStopInLine { get; set; }
+        /// <summary>
+        /// Represents the name of stop in route line
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Represents the unique code of the prev stop
+        /// </summary>
+        public int PrevStop { get; set; }
+        /// <summary>
+        /// Represents the uniqu code of the next stop
+        /// </summary>
+        public int NextStop { get; set; }
+    }
+}
